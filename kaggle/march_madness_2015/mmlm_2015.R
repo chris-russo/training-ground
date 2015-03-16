840b5f45c0adc418a83af7fda283e4221f6c8b05
christopher-russo <christopher@firstaccessmarket.com>
2015-03-16 16:48:37 -0400

    Added March Madness Kaggle competition data, Yhat base R plots.

---
+++ kaggle/march_madness_2015/mmlm_2015.R
@@ -0,0 +1,20 @@
+# Kaggle
+# March Machine Learning Mania 2015
+
+# Created: 16-Mar-2015
+# Updated: 16-Mar-2015
+
+#=============
+# Import data
+#=============
+
+tourney_compact_results <- read.csv(file = './kaggle/march_madness_2015/data/tourney_compact_results.csv')
+seasons <- read.csv(file = './kaggle/march_madness_2015/data/seasons.csv')
+teams <- read.csv(file = './kaggle/march_madness_2015/data/teams.csv')
+tourney_detailed_results <- read.csv(file = './kaggle/march_madness_2015/data/tourney_detailed_results.csv')
+tourney_slots <- read.csv(file = './kaggle/march_madness_2015/data/tourney_slots.csv')
+tourney_seeds <- read.csv(file = './kaggle/march_madness_2015/data/tourney_seeds.csv')
+regular_season_detailed_results <- read.csv(file = './kaggle/march_madness_2015/data/regular_season_detailed_results.csv')
+regular_season_compact_results <- read.csv(file = './kaggle/march_madness_2015/data/regular_season_compact_results.csv')
+sample_submission <- read.csv(file = './kaggle/march_madness_2015/data/sample_submission_2015.csv')
+
