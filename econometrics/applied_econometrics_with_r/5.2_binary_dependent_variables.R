ecba5b72442b7301971ce89b61193d8233f0f084
christopher-russo <christopher@firstaccessmarket.com>
2015-03-17 13:39:45 -0400

    Added 5.2 Binary Dependent Variables example from Applied Econometrics with R.

--- econometrics/applied_econometrics_with_r/5.2_binary_dependent_variables.R
+++ econometrics/applied_econometrics_with_r/5.2_binary_dependent_variables.R
@@ -12,18 +12,20 @@ library(stats)
 library(plyr)
 library(ROCR)
 
-#=============
-# Import data
-#=============
-data('SwissLabor') # Generates df 'SwissLabor'
-
-#========
-# Probit
-#========
-
+#====================================================
 # Female labour force participation in Switzerland
+#====================================================
+
+# Import df 'SwissLabor'
+data('SwissLabor')
+
+# Define equation
 yxb_swiss <- participation ~ . + I(age^2)
+
+# Define model
 swiss_probit <- glm(yxb_swiss, data = SwissLabor, family = binomial(link = 'probit'))
+
+# Coefficient estimates
 summary(swiss_probit)
 
 #===============
@@ -94,4 +96,50 @@ coeftest(swiss_probit, vcov = sandwich)
 #       sandwich estimates are much more useful and less 
 #       controversial in Poisson regressions.
 
+#===========================
 # Quasi-complete separation
+#===========================
+
+# Note: This section discusses problems that occasionally 
+#       arise with probit and logit regressions.
+
+# The goal is to study the deterrent effect of capital 
+# punishment in the United States of America in 1950 utilizing 
+# the MurderRates data taken from Maddala (2001).
+
+# Import df 'MurderRates'
+data('MurderRates')
+
+# Define equation
+yxb_murder <- I(executions > 0) ~ time + income + noncauc + lfp + southern
+
+# Define model
+murder_logit <- glm(yxb_murder, data = MurderRates, family = binomial)
+
+# Note: calling glm() results in a warning message according 
+# to which some fitted probabilities are numerically 
+# identical to zero or one.
+
+# Coefficient estimates
+summary(murder_logit)
+
+# Coefficient estimates with robust standard errors
+# Note: Std. error of southernyes is suspiciously large (2872.2)
+coeftest(murder_logit)
+
+# Define equation
+murder_logit2 <- glm(yxb_murder, 
+                     data = MurderRates, 
+                     family = binomial, 
+                     control = list(epsilon = 1e-15,maxit = 50, trace = FALSE))
+
+# Coefficient estimates with robust standard errors
+# Note: Std. error of southernyes doubled despite 
+#       restrictions on default settings of IWLS algorithm.
+coeftest(murder_logit2)
+
+# For the problem at hand, the change in the coefficient 
+# on southern already indicates that this variable alone 
+# is responsible for the effect.
+table(I(MurderRates$executions > 0), MurderRates$southern)
+
