6d525fc7307d1a1ac0ae41388c06624ed6f0c33f
christopher-russo <christopher@firstaccessmarket.com>
2015-11-11 23:17:28 -0500

    Added NBA lineup simulator script.

---
+++ scripts/nba_lineup_simulator.R
@@ -0,0 +1,95 @@
+# NBA lineup simulator
+
+# This script generates a data frame of all unique player-position combinations
+# for a given 14-player NBA roster.
+
+# Author: CR
+# Created: 2015-11-11
+# Updated: 2015-11-11
+
+# Define list of libraries
+libs <-
+  c("dplyr",
+    "combinat")
+
+# Load libraries
+sapply(libs,
+       require,
+       character.only = TRUE)
+
+# Define list of Knicks players for 2015-16 season
+knicks <-
+  c("Afflalo",
+    "Amundson",
+    "Anthony",
+    "Calderon",
+    "Early",
+    "Galloway",
+    "Grant",
+    "Lopez",
+    "O'Quinn",
+    "Porzingis",
+    "Seraphin",
+    "Thomas",
+    "Vujacic",
+    "Williams")
+
+# Define number of players on the roster
+nRoster <- length(knicks)
+
+# Define number of positions
+nPos <- 5
+
+# Simulate all player-position permutations
+lineup <- 
+  expand.grid(
+    p1 = knicks,
+    p2 = knicks,
+    p3 = knicks,
+    p4 = knicks,
+    p5 = knicks,
+    KEEP.OUT.ATTRS = FALSE,
+    stringsAsFactors = FALSE
+  )
+
+# Generate flag for multiple occurances of values per row
+lineup$dups <- 
+  ifelse(
+    apply(lineup, 1, function(x) length(unlist(table(x)))) < nPos, 
+    1,  # if duplicates exist
+    0)  # if no duplicates exist
+
+# Concatenate positions
+lineup$list <- 
+  with(lineup, 
+       paste(
+         p1, 
+         p2, 
+         p3, 
+         p4, 
+         p5, 
+         sep = ", ")
+       )
+
+# Sort concatenated positions
+lineup$list_sorted <- 
+  as.character(
+    lapply(lineup$list, 
+           function(x) sort(unlist(strsplit(x, split = ", ")))
+           )
+    )
+
+# Remove rows where duplicates exist and drop dups column
+lineup <- 
+  lineup %>% 
+  filter(
+    dups == 0,
+    !duplicated(list_sorted)) %>%
+  select(
+    -dups, 
+    -list, 
+    -list_sorted)
+
+# Check if data sorted properly
+dim(t(combn(14, 5))) == dim(lineup)
+
